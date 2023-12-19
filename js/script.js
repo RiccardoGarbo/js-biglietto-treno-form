550bca746c6ecc513668da6cdb1874017894f201
RiccardoGarbo <Riccardo.garbo1994@gmail.com>
2023-12-19 17:47:26 +0100

    added element to file HTML, and trasfert value to js

---
+++ js/script.js
@@ -0,0 +1,35 @@
+/*
+
+-1 Ricevo tramite form il valore degli elementi elencati
+(Nome e Cognome)(Km da percorrere)(Età del cliente)
+-2 Definisco il prezzo al km
+-3 Definisco lo sconto in base all'età del cliente
+
+
+
+/*Controllo collegamento file js*/
+console.log('JS OK')
+
+// Prendo gli elementi dal file HTML
+const nameSurname = document.getElementById('name-surname')
+console.log(nameSurname.value)
+
+const userKm = document.getElementById('user-km')
+console.log(userKm.value)
+
+const userAge = document.getElementById('user-age')
+console.log(userAge.value)
+
+/*Definisco il prezzo al KM*/
+const priceKm = 0.21
+console.log(priceKm)
+
+
+
+
+
+
+
+
+
+
